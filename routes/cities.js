3660153c09658d22a6d5ea973aeb073d685d7c3e
Seanny Phoenix <seannyphoenix@gmail.com>
2020-04-22 16:09:43 -0700

    set up server, started routes

---
+++ routes/cities.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = exp.router();
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    a: 1
+  })
+});
+
+module.exports = router;
