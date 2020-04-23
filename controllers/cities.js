5b56e59d9ce88c62d6fe4764088c156fb182c98d
KingSri <srirang97@gmail.com>
2020-04-23 15:42:54 -0700

    Editing city controller

--- controllers/cities.js
+++ controllers/cities.js
@@ -2,14 +2,26 @@ const db = require('../models');
 
 
 //All City Shown
-const index = (req, res) => {
-  db.City.find({}, (err, allCity) => {
-    if (err) {
-      return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
-    }
-    res.json(allCity);
-  });
-};
+async function index(req,res){
+  try{
+    let allCity = await db.City.find()
+    res.json(allCity)
+  }
+  catch(err){
+    res.status(500).json({
+      status:500,
+      error: 'Something went wrong, please try again.'
+    })
+  }
+}
+// const index = (req, res) => {
+//   db.City.find({}, (err, allCity) => {
+//     if (err) {
+//       return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
+//     }
+//     res.json(allCity);
+//   });
+// };
 
 //Show City By ID
 async function show(req, res){
@@ -28,14 +40,27 @@ async function show(req, res){
 
 
 //Create New Item (POST)
-const create = (req, res) => {
-  db.City.create(req.body, (err, newCity) => {
-    if (err) {
-      return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
-    }
+
+async function create(req,res){
+  try{
+    let newCity = await db.City.create(req.body);
     res.status(201).json(newCity);
-  });
-};
+  }
+  catch{
+    res.status(500).json({
+      status: 500,
+      error: 'Somethign went wrong, please try again.'
+    })
+  }
+}
+// const create = (req, res) => {
+//   db.City.create(req.body, (err, newCity) => {
+//     if (err) {
+//       return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
+//     }
+//     res.status(201).json(newCity);
+//   });
+// };
 
 
 module.exports = {
