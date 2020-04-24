1de9058e9d0b33ea61675d2242585f38e3f1318f
KingSri <srirang97@gmail.com>
2020-04-23 22:52:24 -0700

    Editing Update function to User to allow user to change city

--- controllers/users.js
+++ controllers/users.js
@@ -14,9 +14,39 @@ const show = (req, res) => {
     })
 }
 
-const update = (req, res) => {
-  res.sendStatus(200);
-}
+//Updating name, city, and post associated with user
+//Broken down into two functions
+
+const updateCity = (req, res) => {
+  try{
+    let foundUser = await db.User.find(req.body.name);
+    if (err){
+      return res.status(400).json({
+        status:400, error: 'Something went wrong, please try again'
+      });
+      //Finding City by Id
+      let cityUpdate = foundUser.user.id(req.params.id);
+      if (!cityUpdate) {
+        return res.status(400).json({status: 400, error: 'City not Found, try again'});
+      }
+      //Changes associated city with user by changing name
+      cityUpdate.name = req.body.name;
+      //saving new city associated with user
+    let savedNewCity = await foundUser.save();
+    //rendering new city
+    let updatingCity = await db.User.findByIdAndUpdate(req.params.userId, req.body, {new: true});
+    if (err){
+      return res.status(400).json({status:400, error: 'Something went wrong, please try again'});
+    }
+    res.json(updatingCity);
+
+  }
+  catch(err)
+  res.status(500).json({
+    status: 500,
+    error: 'Server error.'
+  });
+
 
 module.exports = {
     show,
