3770d5de02963dd6c8e675f930214cc831b0f779
Seanny Phoenix <seannyphoenix@gmail.com>
2020-04-23 23:39:14 -0700

    Created API Docs, removed unneeded API functions

--- controllers/cities.js
+++ controllers/cities.js
@@ -26,20 +26,7 @@ async function show(req, res){
   }
 };
 
-
-//Create New Item (POST)
-const create = (req, res) => {
-  db.City.create(req.body, (err, newCity) => {
-    if (err) {
-      return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
-    }
-    res.status(201).json(newCity);
-  });
-};
-
-
 module.exports = {
   index,
   show,
-  create,
 };
