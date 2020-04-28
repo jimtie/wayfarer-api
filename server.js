24c8d0e0256318337ea9dde803a231ed5b20ee45
jimtie <jimmychen.xin@gmail.com>
2020-04-28 11:43:43 -0700

    all fixed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 // Express Session (Authentication)
 app.use(session({
   store: new MongoStore({
-    url: process.env.MONGO_URI
+    url: process.env.MONGODB_URI
   }),
   secret: process.env.SESSION_SECRET,
   resave: false,
